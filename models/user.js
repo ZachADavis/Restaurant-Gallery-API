6948521f353586e553a92f37d7991f2978dd191f
Owen Andre <owencandre@gmail.com>
2022-10-02 13:06:38 -0400

    updated user model & migration, & added user_email to model

--- models/user.js
+++ models/user.js
@@ -9,18 +9,37 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
-      // define association here
+    static associate(Comment) {
+      // Comment
+      User.belongsToMany(Comment, {
+        foreignKey: "comment_id",
+        as: "comment_id"
+      })
     }
   }
   User.init({
-    user_id: DataTypes.INTEGER,
-    user_email: DataTypes.STRING,
-    user_name: DataTypes.STRING,
-    password: DataTypes.STRING
+    user_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    user_email: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    user_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   }, {
     sequelize,
     modelName: 'User',
+    tableName: 'users',
+    timestamps: false
   });
   return User;
 };
